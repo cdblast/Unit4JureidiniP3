ba75fe94cd9626bccfde49488a9dd381e704afd3
Lucille Amanda Jureidini <145380670+cdblast@users.noreply.github.com>
2024-08-28 11:31:39 -0700

    Lesson 4.2

--- Unit4Per3Jureidini/Assets/Scripts/PlayerController.cs
+++ Unit4Per3Jureidini/Assets/Scripts/PlayerController.cs
@@ -20,6 +20,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
+        
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
 }
