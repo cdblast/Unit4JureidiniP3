bc7698187f450b962846b9002345cd9985ac1834
Lucille Amanda Jureidini <145380670+cdblast@users.noreply.github.com>
2024-09-06 11:46:51 -0700

    PDF step 8 Boss Battle

--- Unit4Per3Jureidini/Assets/Scripts/Enemy.cs
+++ Unit4Per3Jureidini/Assets/Scripts/Enemy.cs
@@ -8,11 +8,25 @@ public class Enemy : MonoBehaviour
     private GameObject player;
     public float speed;
 
+    public bool isBoss = false;
+    
+    public float spawnInterval;
+    private float nextSpawn;
+    
+    public int miniEnemySpawnCount;
+
+    private SpawnManager spawnManager;
+
     // Start is called before the first frame update
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
+
+        if (isBoss)
+        {
+            spawnManager = FindObjectOfType<SpawnManager>();
+        }
     }
 
     // Update is called once per frame
@@ -20,7 +34,16 @@ public class Enemy : MonoBehaviour
     {
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed);
-        
+
+        if (isBoss)
+        {
+            if (Time.time > nextSpawn) 
+            {
+                nextSpawn = Time.time + spawnInterval;
+            }
+
+        }
+
         if (transform.position.y < -10) { Destroy(gameObject); }
     }
 }
--- Unit4Per3Jureidini/Assets/Scripts/SpawnManager.cs
+++ Unit4Per3Jureidini/Assets/Scripts/SpawnManager.cs
@@ -10,6 +10,11 @@ public class SpawnManager : MonoBehaviour
     public int enemyCount;
     public int waveNumber = 1;
 
+    public GameObject bossPrefab;
+    public GameObject[] miniEnemyPrefabs;
+    public int bossRound;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,6 +41,33 @@ public class SpawnManager : MonoBehaviour
         return randomPos;
 
     }
+
+    void SpawnBossWave(int currentRound)
+    {
+        int miniEnemysToSpawn;
+        if (bossRound != 0)
+        {
+            miniEnemysToSpawn = currentRound / bossRound;
+        }
+        else
+        {
+            miniEnemysToSpawn = 1;
+        }
+
+        var boss = Instantiate(bossPrefab, GenerateSpawnPosition(), bossPrefab.transform.rotation);
+        boss.GetComponent<Enemy>().miniEnemySpawnCount = miniEnemysToSpawn;
+    }
+
+    public void SpawnMiniEnemy(int amount)
+    {
+        for(int i = 0; i < amount; i++)
+        {
+            int randomMini = Random.Range(0, miniEnemyPrefabs.Length);
+            Instantiate(miniEnemyPrefabs[randomMini], GenerateSpawnPosition(), miniEnemyPrefabs[randomMini].transform.rotation);
+                
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -43,6 +75,16 @@ public class SpawnManager : MonoBehaviour
         if (enemyCount == 0)
         { 
             waveNumber++; 
+
+            //spawn boss every X number of waves
+            if (waveNumber % bossRound == 0)
+            {
+                SpawnBossWave(waveNumber);
+            }
+            else
+            {
+                SpawnEnemyWave(waveNumber);
+            }
             SpawnEnemyWave(waveNumber);
             int randomPowerup = Random.Range(0, powerupPrefabs.Length);
             Instantiate(powerupPrefabs[randomPowerup], GenerateSpawnPosition(), powerupPrefabs[randomPowerup].transform.rotation);
